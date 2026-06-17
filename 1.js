d33071ee2211946965a2631a75e19fd80b5ac954
kawassaki-lucas <kirossauro@gmail.com>
2026-06-16 22:06:38 -0300

    Implement number collection and display logic
    Add a loop to collect and display positive numbers.

---
+++ 1.js
@@ -0,0 +1,15 @@
+const prompt = require("promp-sync")();
+ 
+let numeros = [];
+let numero;
+let i;
+for(i=0; i<6; i++){
+    //numeros.push = number(prompt("Informe um número:"))
+    numeros[i] = number(prompt("informe um número: "))
+    if(numeros[i]>0){
+        console.log(numeros[i]);
+    }
+    for(i=0; i<6; i++){
+       
+    }
+}
